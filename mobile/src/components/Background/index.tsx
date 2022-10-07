61e6e494909cc7e9b0a68db9b1898a74091a6820
GhostTl <thalys.tauan@ulbra.inf.br>
2022-10-07 03:54:46 -0300

    Adjust on git hidden file

---
+++ mobile/src/components/Background/index.tsx
@@ -0,0 +1,21 @@
+//rnbc
+import { ImageBackground } from 'react-native';
+//@ts-ignore
+import backgroundImg from '../../assets/background-galaxy.png';
+
+import { styles } from './styles';
+
+interface Props {
+    children: React.ReactNode;
+}
+
+export function Background({ children }: Props) {
+  return (
+    <ImageBackground 
+      source={backgroundImg}
+      defaultSource={backgroundImg}
+      style={styles.container}>
+        {children}
+    </ImageBackground>
+  );
+}
